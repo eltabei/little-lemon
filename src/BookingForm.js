8d846d2c617d4f109cba828489fc151ef9927e6e
Eltabei Mohamed <eltabei@users.noreply.github.com>
2025-11-19 11:46:46 +0200

    Clean up BookingForm

--- src/BookingForm.js
+++ src/BookingForm.js
@@ -2,14 +2,8 @@ import { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
 function BookingForm({ availableTimes, dispatch, submitForm }) {
+  const occasions = ["Birthday", "Anniversary"];
   const today = new Date().toISOString().split('T')[0];
-
-  // const [reservationDate, setReservationDate] = useState(today);
-  // const [reservationDate, setReservationDate] = useState("");
-  // const [reservationTime, setReservationTime] = useState(availableTimes[0]);
-  // const [noOfGuests, setNoOfGuests] = useState("1");
-  // const [noOfGuests, setNoOfGuests] = useState("");
-  // const [occasion, setOccasion] = useState("");
 
   const [formData, setFormData] = useState({
     reservationDate: "", reservationTime: availableTimes[1], noOfGuests: "", occasion: ""
@@ -44,30 +38,19 @@ function BookingForm({ availableTimes, dispatch, submitForm }) {
       <form className="reserveForm" ref={formRef} onSubmit={handleSubmit} >
           <label for="res-date">Choose date</label>
           <input type="date" id="res-date" min={today} name="reservationDate" required value={formData.reservationDate} onChange={e => {
-            // setReservationDate(e.target.value);
             handleChange(e);
             dispatch({ type: "reset", payload: formData.reservationTime, date: e.target.value });
           }} />
           <label for="res-time">Choose time</label>
-          <select id="res-time" required name="reservationTime" value={formData.reservationTime} onChange={e => {
-            // setReservationTime(e.target.value);
-            handleChange(e);
-          }}>
+        <select id="res-time" required name="reservationTime" value={formData.reservationTime} onChange={handleChange}>
             {availableTimes.map(t => <option key={t}>{t}</option>)}
           </select>
         <label for="guests">Number of guests</label>
-        <input type="number" required value={formData.noOfGuests} name="noOfGuests" placeholder="1" min="1" max="10" id="guests" onChange={e => {
-          // setNoOfGuests(e.target.value)
-          handleChange(e);
-        }} />
+        <input type="number" required value={formData.noOfGuests} name="noOfGuests" placeholder="1" min="1" max="10" id="guests" onChange={handleChange} />
         <label for="occasion">Occasion</label>
-        <select id="occasion" value={formData.occasion} name="occasion" onChange={e => {
-          // setOccasion(e.target.value)
-          handleChange(e);
-        }}>
+        <select id="occasion" value={formData.occasion} name="occasion" onChange={handleChange}>
           <option></option>
-          <option>Birthday</option>
-          <option>Anniversary</option>
+          {occasions.map(o => (<option key={o} value={o}>{o}</option>))}
         </select>
         <input type="submit" value="Make your reservation" disabled={!isFormValid} />
       </form>
