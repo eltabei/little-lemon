f891e669ec38a703a53f5bc8f39549e07fbec0b5
Eltabei Mohamed <eltabei@users.noreply.github.com>
2025-11-19 11:39:48 +0200

    Group useState for all form fields into formData

--- src/BookingForm.js
+++ src/BookingForm.js
@@ -3,26 +3,36 @@ import { useNavigate } from "react-router-dom";
 
 function BookingForm({ availableTimes, dispatch, submitForm }) {
   const today = new Date().toISOString().split('T')[0];
+
   // const [reservationDate, setReservationDate] = useState(today);
-  const [reservationDate, setReservationDate] = useState("");
-  const [reservationTime, setReservationTime] = useState(availableTimes[0]);
+  // const [reservationDate, setReservationDate] = useState("");
+  // const [reservationTime, setReservationTime] = useState(availableTimes[0]);
   // const [noOfGuests, setNoOfGuests] = useState("1");
-  const [noOfGuests, setNoOfGuests] = useState("");
-  const [occasion, setOccasion] = useState("");
+  // const [noOfGuests, setNoOfGuests] = useState("");
+  // const [occasion, setOccasion] = useState("");
+
+  const [formData, setFormData] = useState({
+    reservationDate: "", reservationTime: availableTimes[1], noOfGuests: "", occasion: ""
+  });
+
   const [isFormValid, setIsFormValid] = useState(false);
   const formRef = useRef(null);
   const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
-    dispatch({type: "remove", payload: reservationTime, date: reservationDate });
-    const formData = { reservationDate, reservationTime, noOfGuests, occasion };
+    dispatch({type: "remove", payload: formData.reservationTime, date: formData.reservationDate });
     if (submitForm(formData)) {
       navigate("/confirm", { state: formData });
     }
   }
 
-  function handleChange() {
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setFormData(prev => ({
+      ...prev,
+      [name]: value
+    }))
     if (formRef.current) {
       const isValid = formRef.current.checkValidity();
       setIsFormValid(isValid);
@@ -33,27 +43,27 @@ function BookingForm({ availableTimes, dispatch, submitForm }) {
       <>
       <form className="reserveForm" ref={formRef} onSubmit={handleSubmit} >
           <label for="res-date">Choose date</label>
-          <input type="date" id="res-date" min={today} required value={reservationDate} onChange={e => {
-            setReservationDate(e.target.value);
-            handleChange();
-            dispatch({ type: "reset", payload: reservationTime, date: e.target.value });
+          <input type="date" id="res-date" min={today} name="reservationDate" required value={formData.reservationDate} onChange={e => {
+            // setReservationDate(e.target.value);
+            handleChange(e);
+            dispatch({ type: "reset", payload: formData.reservationTime, date: e.target.value });
           }} />
           <label for="res-time">Choose time</label>
-        <select id="res-time" required value={reservationTime} onChange={e => {
-          setReservationTime(e.target.value);
-          handleChange();
-        }}>
+          <select id="res-time" required name="reservationTime" value={formData.reservationTime} onChange={e => {
+            // setReservationTime(e.target.value);
+            handleChange(e);
+          }}>
             {availableTimes.map(t => <option key={t}>{t}</option>)}
           </select>
         <label for="guests">Number of guests</label>
-        <input type="number" required value={noOfGuests} placeholder="1" min="1" max="10" id="guests" onChange={e => {
-          setNoOfGuests(e.target.value)
-          handleChange();
+        <input type="number" required value={formData.noOfGuests} name="noOfGuests" placeholder="1" min="1" max="10" id="guests" onChange={e => {
+          // setNoOfGuests(e.target.value)
+          handleChange(e);
         }} />
         <label for="occasion">Occasion</label>
-        <select id="occasion" value={occasion} onChange={e => {
-          setOccasion(e.target.value)
-          handleChange();
+        <select id="occasion" value={formData.occasion} name="occasion" onChange={e => {
+          // setOccasion(e.target.value)
+          handleChange(e);
         }}>
           <option></option>
           <option>Birthday</option>
